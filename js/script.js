cc606b0c767fbcd82e813c931427bf680bbc3bea
cmchap <corymchap@gmail.com>
2016-03-01 12:20:03 -0500

    added map cover
    When the user scrolls down the page, there is a delay on hover before the map begins to zoom.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,58 @@
-function scrollNav(){$(".navbar-brand, .nav a").click(function(a){return"none"==$(".navbar-toggle").css("display")||$(this).hasClass("navbar-brand")||$(".navbar-toggle").trigger("click"),$(this).blur(),$("html, body").stop().animate({scrollTop:$($(this).attr("href")).offset().top-$("body").attr("data-offset")},400),!1})}$(document).ready(function(){scrollNav(),$(".navbar-brand").click(function(a){var n=$(".navbar-collapse"),l=n.hasClass("in");l===!0&&n.collapse("hide")}),$(document).click(function(a){var n=$(a.target),l=$(".navbar-collapse"),s=l.hasClass("in");s!==!0||n.hasClass("navbar-toggle")||l.collapse("hide")})});
+function scrollNav() {
+	$('.navbar-brand, .nav a').click(function(event) {
+		if ($('.navbar-toggle').css('display') != 'none' && (!$(this).hasClass('navbar-brand'))) {
+			$(".navbar-toggle").trigger("click");
+		}
+		$(this).blur();
+		$('html, body').stop().animate({
+			scrollTop: $($(this).attr('href')).offset().top - $('body').attr("data-offset")
+		}, 400);
+		return false;
+	});
+}
+
+$(document).ready(function() {
+	scrollNav();
+	$('.site-title').click(function(event) {
+		$('html, body').stop().animate({
+				scrollTop: 0}, 400);
+			return false;
+	});
+	$('.navbar-brand').click(function(event) {
+		var $navbar = $(".navbar-collapse");
+		var _opened = $navbar.hasClass("in");
+		if (_opened === true) {
+			$navbar.collapse('hide');
+		}
+	});
+	$(document).click(function(event) {
+		var clickover = $(event.target);
+		var $navbar = $(".navbar-collapse");
+		var _opened = $navbar.hasClass("in");
+		if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+			$navbar.collapse('hide');
+		}
+	});
+
+	var timeout;
+	// if (Modernizr.touch){
+	// 	$('.map-cover').hide();
+	// } else {
+		$('.mapbox').toggleClass("map-desaturated");
+	    $('.map-container').on('mouseenter', function() {
+			if (timeout !== null) {clearTimeout(timeout);}
+			timeout = setTimeout(function() {
+				$('.map-cover').hide();
+			}, 500);
+			$('.mapbox').toggleClass("map-saturated");
+		});
+
+		$('.map-container').on('mouseleave', function() {
+			if (timeout !== null) {clearTimeout(timeout);}
+			timeout = setTimeout(function() {
+				$('.map-cover').show();
+			}, 200);
+			$('.mapbox').toggleClass("map-saturated");
+		});
+	// }
+});
